65ec7fd9b28fd28d0634213a23976ce27b274500
Shawn M Moore <sartak@bestpractical.com>
2011-06-10 16:48:55 -0400

    Override grand_search_init to use Pod::Cpandoc::Scraper

--- lib/Pod/Cpandoc.pm
+++ lib/Pod/Cpandoc.pm
@@ -2,8 +2,26 @@ package Pod::Cpandoc;
 use strict;
 use warnings;
 use base 'Pod::Perldoc';
+use Pod::Cpandoc::Scraper;
 
 our $VERSION = '0.01';
+
+use constant scraper => 'Pod::Cpandoc::Scraper';
+
+sub grand_search_init {
+    my $self = shift;
+    my @found = $self->SUPER::grand_search_init(@_);
+
+    if (@found == 0) {
+        my $pages = shift;
+
+        for my $module (@$pages) {
+            push @found, $self->scraper->get_documentation_for($module);
+        }
+    }
+
+    return @found;
+}
 
 1;
 
