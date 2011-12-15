2c336d9e4085203095026f6eade04008ebe391c0
Shawn M Moore <sartak@gmail.com>
2011-12-15 12:23:39 -0500

    Link to perl advent article

--- lib/Pod/Cpandoc.pm
+++ lib/Pod/Cpandoc.pm
@@ -193,6 +193,8 @@ The sneaky install was inspired by L<https://github.com/defunkt/hub>.
 
 L<http://tech.bayashi.jp/archives/entry/perl-module/2011/003305.html>
 
+L<http://perladvent.org/2011/2011-12-15.html>
+
 L<http://sartak.org/talks/yapc-na-2011/cpandoc/>
 
 =head1 AUTHOR
